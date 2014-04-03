8b276aca3c38c8e37c2874bd5f574d3c8dbd2cd7
Paul Czarkowski <paul.czarkowski@rackspace.com>
2014-04-03 18:34:48 -0500

    rubocop

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 # Encoding: utf-8
 name             'docker-demo-chef'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'none'
-license          ''
+maintainer       'paul'
+maintainer_email 'me@here.com'
+license          'Apache2'
 description      'Installs/Configures docker-demo-chef'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
